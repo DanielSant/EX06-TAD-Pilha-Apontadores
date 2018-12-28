01ef70dc6e61862a1fa8cbc6387db167d6f75a18
Daniel Sants <danielsantos619@yahoo.com.br>
2018-12-28 14:13:24 -0300

    Estrutura principal imposta, feitas funções cria() e menu()

--- Pilha_Apontadores.h
+++ Pilha_Apontadores.h
@@ -1,8 +1,15 @@
 #ifndef PILHA_APONTADORES_H_INCLUDED
 #define PILHA_APONTADORES_H_INCLUDED
 
+// =----------------------------------
+
 #include <iostream>
 using namespace std;
+#include <stdlib.h>
+#include <conio2.h>
+#include <stdio.h>
+
+// =----------------------------------
 
 typedef struct nodoPilha *apontador;
 
@@ -20,5 +27,46 @@ typedef struct {
     struct nodoPilha *primeiro;
     struct nodoPilha *ultimo;
 } TPilha;
+
+enum opcoes
+{
+    CRIA, EMPILHA, DESEMPILHA,
+    TOPO, IMPRIME, FIM
+};
+
+// =----------------------------------
+
+void cria(TPilha& pilha);
+opcoes menu();
+bool vazia(TPilha& pilha);
+void empilha(TPilha& pilha, TInfo item);
+void desempilha(TPilha& pilha);
+void topo(TPilha& pilha);
+void imprime(TPilha& pilha);
+
+// =----------------------------------
+
+void cria(TPilha& pilha)
+{
+    pilha.primeiro = (apontador) malloc(sizeof(struct nodoPilha));
+    pilha.ultimo = pilha.primeiro;
+    pilha.primeiro->proximo = NULL;
+}
+
+opcoes menu()
+{
+    int o;
+    clrscr();
+    cout << "\n Menu:" << endl
+        << " 1 - Empilhar\n"
+        << " 2 - Desempilhar\n"
+        << " 3 - Ver Topo\n"
+        << " 4 - Imprimir\n"
+        << " 5 - Sair\n"
+        << " Digite uma opção: ";
+    scanf("%d", &o);
+    //fflush(stdin);
+    return (opcoes) o;
+}
 
 #endif // PILHA_APONTADORES_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include "Pilha_Apontadores.h"
+#include <locale.h>
+#include <windows.h>
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    setlocale(LC_ALL, "portuguese");
+    opcoes opc;
+
+    while((opc = menu()) != FIM)
+    {
+        switch(opc)
+        {
+            case EMPILHA:
+                //empilha();
+                break;
+            case DESEMPILHA:
+                //desempilha();
+                break;
+            case TOPO:
+                //topo();
+                break;
+            case IMPRIME:
+                //imprime();
+                break;
+            default:
+                cout << "\n Opção inválida!\n";
+                Sleep(2000);
+        }
+    }
+
     return 0;
 }
